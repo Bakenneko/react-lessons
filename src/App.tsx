0925858cb61a9e10ff411ebaac3a3b2ded9a17f5
Andrew Kurylyk <andronniy7@gmail.com>
2025-04-27 12:41:13 +0300

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import './App.css';
+import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-
-
+      <MyComponent text={'hello 1'}/>
+      <MyComponent text={'hello 2'}/>
+      <MyComponent text={'hello Andrew'}/>
     </>
   );
 }
