046e60c1fa1eed9b4ff389d82a24733805514e34
Курилик А.І <andronniy7@gmail.com>
2025-05-02 13:03:18 +0300

    one

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
 import './App.css';
 
-function App() {
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
+
+const App = () => {
 
   return (
-    <>
-      <ul>
-          <li>Hello</li>
-          <li>Hi</li>
-          <li>Good</li>
-          <li>Day</li>
-          <li>Nice</li>
-      </ul>
-    </>
+    <div className="container">
+        <ul className="course-list">
+
+            {coursesTitleArray.map((value:string, index:number) => (
+                <li key={index} className="course-item">
+                    {value}</li>
+            ))}
+        </ul>
+    </div>
   );
 }
 
 export default App
+
+
+
+
+// Тест системи
+// import './App.css';
+//
+// function App() {
+//
+//   return (
+//     <>
+//       <ul>
+//           <li>Hello</li>
+//           <li>Hi</li>
+//           <li>Good</li>
+//           <li>Day</li>
+//           <li>Nice</li>
+//       </ul>
+//     </>
+//   );
+// }
+//
+// export default App
