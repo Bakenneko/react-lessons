5595f1529cf9e0fbf38bc7382e495c586f7cc00c
Andrew Kurylyk <andronniy7@gmail.com>
2025-06-15 17:51:39 +0300

    15.06.25

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
 import {IUserWithTokens} from "../models/auth/IUserWithTokens.ts";
-
-
-
-const axiosInstance = axios.create({
-    baseURL: 'https://dummyjson.com/auth',
-    headers: {}
-});
+import {IProduct} from "../models/auth/IProduct.ts";
+import {IProductsResponseModelType} from "../models/auth/IProductsResponseModel.ts";
+import {retriveLocalStorage} from "./helpers.ts";
 
 type LoginData = {
     username: string;
@@ -14,8 +10,28 @@ type LoginData = {
     expireInMins: number
 }
 
-export const login = async ({username,password,expireInMins}:LoginData) => {
+const axiosInstance = axios.create({
+    baseURL: 'https://dummyjson.com/auth',
+    headers: {}
+});
+
+
+axiosInstance.interceptors.request.use((requestObject)=>{
+    if (requestObject.method?.toUpperCase() === "GET") {
+        requestObject.headers.Authorization = 'Bearer ' + retriveLocalStorage<IUserWithTokens>('user').accessToken
+    }
+    return requestObject;
+})
+
+
+export const login = async ({username,password,expireInMins}:LoginData): Promise<IUserWithTokens> => {
     const {data:userWithTokens}= await axiosInstance.post<IUserWithTokens>('/login',{username,password,expireInMins});
     console.log(userWithTokens);
     localStorage.setItem('user', JSON.stringify(userWithTokens));
+    return userWithTokens;
+}
+
+export const loadAuthProducts = async ():Promise<IProduct[]> => {
+    const {data: {products}}= await axiosInstance.get<IProductsResponseModelType>('/products');
+    return products
 }
